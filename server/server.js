2b393ab182db9e21cf03cd70b0628de311fb23b8
alex <alexanderjannespia0127@gmail.com>
2025-10-08 22:01:37 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,53 +1,54 @@
-const express = require('express');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-
-
-const mongoUrl = process.env.MONGO_URL;
-const dbName = 'its';
-let db;
-
 app.use(cors());
 
-async function connectDB() {
-	if (!client) {
-		client = await MongoClient.connect(mongoUrl);
-		db = client.db(dbName);
-		console.log('Connected to MongoDB');
-	}
-	return db;
+const mongoUrl = process.env.MONGO_URL;
+const dbName = "its";
+
+let cachedClient = null;
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedDb) return cachedDb;
+  const client = await MongoClient.connect(mongoUrl);
+  cachedClient = client;
+  cachedDb = client.db(dbName);
+  return cachedDb;
 }
 
-app.get('/', (req, res) => {
-	res.send('Hello World from Vercel!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/stop/:name', async (req, res) => {
-	try {
-		const db = await connectDB();
+app.get("/api/stop/:name", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const stop = await db.collection("its").findOne({ _id: req.params.name });
 
-		const stop = await db.collection('its').findOne({ _id: req.params.name });
-		if (!stop) return res.status(404).json({ error: 'Stop not found' });
+    if (!stop) return res.status(404).json({ error: "Stop not found" });
 
-		res.json({
-			passengerCount: stop.passengerCount,
-			lastUpdated: stop.lastUpdated,
-		});
-	} catch (err) {
-		console.error('Error:', err);
-		res.status(500).json({ error: err.message });
-	}
+    res.json({
+      passengerCount: stop.passengerCount,
+      lastUpdated: stop.lastUpdated,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
+
+module.exports = app;
 
 /*
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
 });*/
 
-module.exports = app;
+
 
 /*
 
